cbb11b75eedecd15b1b52abbbe039af4042437e9
Jacek Mikołajczak <mikolajczak.jaca@gmail.com>
2018-03-14 01:09:53 +0100

    Add #first_name and #last_name to new user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 # :nodoc
 class User < ApplicationRecord
+  validates :first_name, :last_name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
 end
