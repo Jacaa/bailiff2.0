b6c2d2b3379fdc3e503f043135e7a17b28afdf7f
Jacek Mikołajczak <mikolajczak.jaca@gmail.com>
2018-03-16 02:54:51 +0100

    Improve layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'dashboard#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources  :users, only: [:index, :show] do
-    resources :debts, only: [:create]
-    get '/history', to: 'debts#history'
+    get '/history',        to: 'debts#history'
+    post '/',              to: 'debts#create',         as: 'create_debt'
     put '/cover/:debt_id', to: 'debts#set_as_covered', as: 'cover'
   end
 end
