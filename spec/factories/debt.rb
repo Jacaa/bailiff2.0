519e46038ecb7376130c0dd55fd0a6a79083026d
Jacek Mikołajczak <mikolajczak.jaca@gmail.com>
2018-03-16 10:21:41 +0100

    Fix users controller tests

--- spec/factories/debt.rb
+++ spec/factories/debt.rb
@@ -2,9 +2,12 @@
 
 FactoryBot.define do
   factory :debt do
+    debtor_id   1
+    creditor_id 2
     amount      2.00
     purpose     'For a snack'
     date        { Time.zone.now }
     deadline    { Time.zone.now + 10 }
+    paid        false
   end
 end
--- spec/models/debt_spec.rb
+++ spec/models/debt_spec.rb
@@ -13,19 +13,31 @@ RSpec.describe Debt, type: :model do
   it { should validate_presence_of(:amount) }
   it { should validate_numericality_of(:amount).is_greater_than(0) }
 
+  let(:users) { create_list(:user, 2) }
+
   describe '#deadline_is_possible?' do
-    let(:users) { create_list(:user, 2) }
-    let(:debt) do
-      build(:debt,
-            deadline:    1.hour.ago,
-            debtor_id:   users.first.id,
-            creditor_id: users.last.id)
-    end
+    let(:debt) { build(:debt, deadline: 1.hour.ago) }
 
     it { expect(debt).to be_invalid }
     it 'returns errors message' do
       debt.save
       expect(debt.errors[:deadline]).to eq(['must be later than date'])
+    end
+  end
+
+  describe 'scopes' do
+    let(:debt) do
+      create(:debt, debtor_id:   users.first.id,
+                    creditor_id: users.second.id,
+                    created_at:  1.hour.ago)
+    end
+    let(:debt_later) do
+      create(:debt, creditor_id: users.first.id, debtor_id: users.first.id)
+    end
+    context '-> default scope' do
+      it 'returns debts in descending order by created_at' do
+        expect(Debt.all).to eq([debt_later, debt])
+      end
     end
   end
 end
